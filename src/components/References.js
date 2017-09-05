854c9b8740209f2d97f1e172a5be631108ecdc09
Georgia Tardy <georgiatardyart@gmail.com>
2017-09-05 15:02:08 -0400

    added more content to pages

--- src/components/References.js
+++ src/components/References.js
@@ -11,10 +11,19 @@ export default class References extends Component {
 
        <div className="references_info">
          <h2>Name</h2>
-         <p>Title</p>
+
+         <p>Melanie Beelen</p>
+         <p>Title: Executive Director, Baxter Community Center</p>
          <a href=""></a>
-         <p>Phone</p>
-         <p>Email</p>
+         <p>Phone: 616-456-8593</p>
+         <p>Email: melanieb@baxtercommunitycenter.org</p>
+         <br></br>
+
+         <p>Bill Holsinger-Robinson</p>
+         <p>Title: Director, TEDx Grand Rapids</p>
+         <a href="https://www.facebook.com/TEDxGrandRapids/"></a>
+         <p>Phone: 616-555-5555</p>
+
 
        </div>
      </section>
